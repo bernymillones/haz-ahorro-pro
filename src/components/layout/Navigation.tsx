a4f9be0268951486799ed817845c29fb82e77973
Bernardo Rodriguez Rosales <62794203+bernymillones@users.noreply.github.com>
2025-10-11 19:17:38 -0600

    Commit 6 Navigation.tsx
    Se cambio el emoji en el logo porque modifique en loveble y se cambio.

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -28,7 +28,7 @@ export default function Navigation() {
             <div className="text-2xl font-bold">
               <span className="text-primary">Haz</span>
               <span className="text-foreground">Dinero</span>
-              <span className="text-muted-foreground text-3xl">.Online</span>
+              <span className="text-muted-foreground text-sm"> 💰</span>
             </div>
           </Link>
 
